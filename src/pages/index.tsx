25d66304db73be6b9a5d4d8f5959e145764a5677
marinetteclemente <162115251+marinetteclemente@users.noreply.github.com>
2024-03-17 22:15:29 +1100

    update-index-v7.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -302,17 +302,26 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
           <br />
         </div>
         
-        <Malleable id="explanation-2" isActive={isEdit} edits={edits}>
-          When people visit this site, the response always comes instantly from
-          their{' '}
-          <a
-            target="_blank"
-            rel="noopener"
-            href="https://vercel.com/docs/concepts/edge-network/overview"
+        <Malleable as="h2" id="title-2" isActive={isEdit} edits={edits}>
+          What do you want to learn or do more of at work? <br />
+          <Malleable
+            id="explanation-1-inspect"
+            as="span"
+            isActive={isEdit}
+            edits={edits}
           >
-            nearest location
-          </a>
-          .
+            Technical skills<br />
+            Specifically in the fields of modern web architecture. I have been involved working in SaaS platform and I really find it interesting on how the web architecture works. <br />
+            Being involved in understanding the frontend frameworks like Next.js and React, cloud technologies, serverless computing and DNS would really be a good opportunity for me to dive in. <br />
+            Strengthening my technical expertise will enable me to contribute more effectively to projects and problem-solving initiatives.<br />
+            
+            Communication Skills <br />
+           I would also like to further improve my communication skills. <br />
+           It is crucial being a CSE, this includes relaying complex ideas effectively to the team and other stakeholders. <br />
+      
+          </Malleable>
+          <br />
+          
         </Malleable>
         <Malleable id="explanation-3" isActive={isEdit} edits={edits}>
           Unlike traditional static solutions, however, you can generate
