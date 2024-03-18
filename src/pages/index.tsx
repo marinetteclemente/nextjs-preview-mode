05679d68972678902aa8caa906f9fc925f17a44e
marinetteclemente <162115251+marinetteclemente@users.noreply.github.com>
2024-03-18 19:57:46 +1100

    update-index-v15.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -297,22 +297,17 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             3. Dig through logs to troubleshoot a customer's broken project: Can be time-consuming and challenging. Most of the time it requires high-level technical expertise<br />
             4. Work with 3rd party partners to track down a tricky situation for a joint customer: the lack of direct control over external parties<br />
             5. Engage multiple users at once in a public discussion, to answer their questions and troubleshoot problems: Can be challenging to balance the needs of different users <br />
-            
-          </Malleable>
-          <br />
-        </div>
-        
-        <div className="explanation">
-        <div className="p">
+
           <Malleable as="h3" id="title-3" isActive={isEdit} edits={edits}>
           What do you want to learn or do more of at work? <br/>
-           </Malleable>
-          <Malleable
+          </Malleable>
+            
+            <Malleable
             id="explanation-2-inspect"
             as="span"
             isActive={isEdit}
             edits={edits}
-          >
+            >
             Technical skills<br />
             Specifically in the fields of modern web architecture. I have been involved working in SaaS platform and I really find it interesting on how the web architecture works. <br />
             Being involved in understanding the frontend frameworks like Next.js and React, cloud technologies, serverless computing and DNS would really be a good opportunity for me to dive in. <br />
@@ -321,10 +316,11 @@ function Content({ isEdit, edits }: { isEdit: boolean; edits: FieldEdit[] }) {
             Communication Skills <br />
            I would also like to further improve my communication skills. <br />
            It is crucial being a CSE, this includes relaying complex ideas effectively to the team and other stakeholders. <br />
-          
         </Malleable>
         <br />
-      </div>
+          </Malleable>
+          <br />
+        </div>
           
         <Malleable id="explanation-3" isActive={isEdit} edits={edits}>
           Unlike traditional static solutions, however, you can generate
